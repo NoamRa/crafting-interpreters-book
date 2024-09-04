9db768980f21962875b2fc2f828c8ac1a59e3de5
NoamR <noam.raby@up42.com>
2024-09-04 10:40:47 +0200

    enclosing environment

--- src/parsing/Environment.ts
+++ src/parsing/Environment.ts
@@ -4,13 +4,19 @@ import { LiteralValue } from "../scanning/types.ts";
 
 export class Environment {
   private values = new Map<string, LiteralValue>();
+  private enclosing: Environment | null;
+
+  constructor(enclosing?: Environment) {
+    this.enclosing = enclosing ?? null;
+  }
 
   define(name: string, value: LiteralValue) {
     this.values.set(name, value);
   }
 
-  get(name: Token) {
+  get(name: Token): LiteralValue | undefined {
     if (this.values.has(name.lexeme)) return this.values.get(name.lexeme);
+    if (this.enclosing !== null) return this.enclosing.get(name);
 
     throw new RuntimeError(name, `Undefined variable '${name.lexeme}'`);
   }
@@ -18,6 +24,11 @@ export class Environment {
   assign(name: Token, value: LiteralValue) {
     if (this.values.has(name.lexeme)) {
       this.values.set(name.lexeme, value);
+      return;
+    }
+
+    if (this.enclosing !== null) {
+      this.enclosing.assign(name, value);
       return;
     }
 
